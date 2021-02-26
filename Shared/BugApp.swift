6a38b690302a05fe9b09994844c5a1db799312ae
Ian Dundas <contact@iandundas.com>
2021-02-26 19:18:46 +0100

    Initial Commit

---
+++ Shared/BugApp.swift
@@ -0,0 +1,17 @@
+//
+//  BugApp.swift
+//  Shared
+//
+//  Created by Ian Dundas on 26/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct BugApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
